4a00d33cd87b09c82314167a13ad193b3abcbfee
DAWEI QIN <atpergyeye@gmail.com>
2025-10-05 01:50:16 -0400

    Initial commit

---
+++ ApplicationCore/Contracts/Services/IMovieService.cs
@@ -0,0 +1,21 @@
+﻿using ApplicationCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Contracts.Services
+{
+    public interface IMovieService
+    {
+        List<MovieCard> Get30HighestGrossingMovies() {
+            var movies = new List<MovieCard>
+            {
+                new MovieCard { Title = "Inception", Id = 1, PosterUrl = "1"},
+                new MovieCard { Title = "Interstellar", Id = 2, PosterUrl = "2"}
+            };
+            return movies;
+        }
+    }
+}
---
+++ Infrastructure/Services/MovieService.cs
@@ -0,0 +1,22 @@
+﻿using ApplicationCore.Contracts.Services;
+using ApplicationCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services
+{
+    public class MovieService : IMovieService
+    {
+        public List<MovieCard> Get30HighestGrossingMovies() {
+            var movies = new List<MovieCard>
+            {
+                new MovieCard { Title = "Inception", Id = 1, PosterUrl = "1"},
+                new MovieCard { Title = "Interstellar", Id = 2, PosterUrl = "2"}
+            };
+            return movies;
+        }
+    }
+}
