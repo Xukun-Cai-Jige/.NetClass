3dff6059eaebb1c6e1060bca5a4f4e9419fbe200
DAWEI QIN <atpergyeye@gmail.com>
2025-10-07 03:52:58 -0400

    Initial commit or update

---
+++ Infrastructure/Migrations/20251007020205_setupreviewdecimal.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class setupreviewdecimal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Rating",
+                table: "Review",
+                type: "decimal(3,2)",
+                nullable: false,
+                defaultValue: 9.9m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Rating",
+                table: "Review",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(3,2)",
+                oldDefaultValue: 9.9m);
+        }
+    }
+}
--- MovieShopMVC/Program.cs
+++ MovieShopMVC/Program.cs
@@ -1,11 +1,15 @@
 using ApplicationCore.Contracts.Services;
+using Infrastructure.Data;
 using Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<IMovieService, MovieServiceMock>();
+builder.Services.AddDbContext<MovieShopDbContext>(options=>
+{ options.UseSqlServer(builder.Configuration.GetConnectionString("MovieShopDbConnection")); });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
